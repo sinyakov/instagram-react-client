14d66c8da20f689e92abbdcb86a37038289d9db8
Max Sinyakov <max@sinyakov.com>
2017-09-25 16:04:30 +0300

    migrate from css to styled-components in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,66 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from '../img/logo.png';
 
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 0;
+  text-align: center;
+  font-family: 'Neucha', cursive;
+`;
+
+const Logo = styled.img`
+  display: inline-block;
+  width: 4rem;
+  margin: 0 0.5rem;
+  transition: transform 0.75s ease-in-out;
+
+  @media (max-width: 40rem) {
+    width: 3.5rem;
+    margin: 0 0.5rem 0 0;
+  }
+
+  &:hover {
+    transform: rotateZ(360deg);
+  }
+`;
+
+const Text = styled.div``;
+
+const Title = styled.h1`
+  display: inline-block;
+  margin: 0;
+  font-size: 2.25rem;
+  line-height: 1.1;
+
+  @media (max-width: 40rem) {
+    font-size: 2rem;
+  }
+`;
+
+const Tagline = styled.p`
+  margin: 0;
+  font-size: 1.125em;
+`;
+
+const Link = styled.a`
+  color: #44a;
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
 export default () => (
-  <header className="main-header">
-    <img src={logo} className="main-header__logo" alt="Logo" />
-    <div className="main-header__text">
-      <h1 className="main-header__title">Факты о котиках</h1>
-      <p className="main-header__tagline">
+  <Wrapper>
+    <Logo src={logo} alt="Logo" />
+    <Text>
+      <Title>Факты о котиках</Title>
+      <Tagline>
         Из инстаграма&nbsp;
-        <a className="main-header__link" href="https://instagram.com/catsfromreddit">
-          @catsfromreddit
-        </a>
-      </p>
-    </div>
-  </header>
+        <Link href="https://instagram.com/catsfromreddit">@catsfromreddit</Link>
+      </Tagline>
+    </Text>
+  </Wrapper>
 );
