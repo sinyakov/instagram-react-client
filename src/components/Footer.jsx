1c9622a674792db1063aee9c1426c48b3fc11c0a
Max Sinyakov <max@sinyakov.com>
2017-09-25 15:56:47 +0300

    migrate from css to styled-components in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default () => <footer className="main-footer">Max Sinyakov © 2k17</footer>;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 1rem 0;
+  color: #666;
+  font-size: 0.75rem;
+  line-height: 1.5;
+`;
+
+const Copyright = styled.div``;
+const About = styled.div``;
+
+const Link = styled.a`
+  color: #666;
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+export default () => (
+  <Wrapper>
+    <Copyright>
+      <Link href="http://sinyakov.com">Максим Синяков</Link> (c) 2к17
+    </Copyright>
+    <About>
+      <Link href="https://github.com/sinyakov/instagram-react-client">Тестовое задание</Link>
+      <span>&nbsp;для&nbsp;</span>
+      <Link href="https://digdes.ru">Digital Design</Link>
+    </About>
+  </Wrapper>
+);
